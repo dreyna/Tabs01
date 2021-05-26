c74ac63442f5419a7661ba27776714a8efafd411
dreyna <reyna_b@upeu.edu.pe>
2021-05-25 22:04:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tabs01/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tabs01;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
